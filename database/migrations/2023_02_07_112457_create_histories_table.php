3f507389e7c709377714138a7bf458ca328b1a5d
Aaron Mangan <azza.mangan@gmail.com>
2023-02-07 21:32:38 +1000

    dev bootstrapped histories

---
+++ database/migrations/2023_02_07_112457_create_histories_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('histories', function (Blueprint $table) {
+            $table->id();
+            $table->string('model');
+            $table->int('model_id');
+            $table->int('user_id');
+            $table->string('event', 100);
+            $table->string('level');
+            $table->json('old')->nullable();
+            $table->json('new');
+            $table->string('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('histories');
+    }
+};
